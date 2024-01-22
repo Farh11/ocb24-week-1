8eba31b7d3ef3fee3c3e9db959bf9a78f558c9da
Farh11 <fhnnhkim@gmail.com>
2024-01-22 23:16:35 +0800

    Delete some files

--- Untitled-1.js
+++
@@ -1,9 +0,0 @@
-<button onclick = "toggleContent()">Toggle Content</button>
-
-
-<script>
-    function toggleContent() {
-        var content = document.querySelector('.center-box');
-        content.style.display = (content.style.display === 'none' || content.style.display === '') ? 'block' : 'none';
-    }
-</script>
